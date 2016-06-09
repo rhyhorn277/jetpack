97b1f9f41d358bf6453437e4bb941fefa91d7b14
Igor Zinovyev <zinigor@gmail.com>
2016-06-10 00:20:53 +0300

    Added NVM commands that will actually make Travis use Node 5.

--- tests/load-travis.sh
+++ tests/load-travis.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 if [ "$WP_TRAVISCI" != "phpunit" ]; then
-	gem install sass
-	gem install compass
-	nvm install 5
-	npm install -g npm@'3.8.9'
-	npm install -g gulp-cli
-	npm install
+    gem install sass
+    gem install compass
+    source ~/.nvm/nvm.sh
+    nvm install 5.11
+    nvm use 5.11
+    npm install -g npm
+    node --version
+    npm --version
+    nvm --version
+    npm install -g gulp-cli
+    npm install
 fi
