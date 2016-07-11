437b4fc4dca2c254ef0ac9d099e7b95fd86c7ff5
Igor Zinovyev <zinigor@gmail.com>
2016-07-11 16:45:28 +0300

    Using NPM outside of the loader bash script.

--- tests/load-travis.sh
+++ tests/load-travis.sh
@@ -3,12 +3,7 @@
 if [ "$WP_TRAVISCI" != "phpunit" ]; then
     gem install sass
     gem install compass
-    source ~/.nvm/nvm.sh
-    nvm install 5.11
-    nvm use 5.11
     npm install -g npm
-    node --version
-    npm --version
     npm install -g gulp-cli
     npm install
 fi
